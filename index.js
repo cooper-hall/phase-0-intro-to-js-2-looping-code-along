b7c8bb4c3a91392000d99e35a4a888ec6171072d
Cooper Hall <cooperhall111@gmail.com>
2022-10-12 20:03:22 -0400

    commit tree d5452335705019011e634c823fa993df342a4eac

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const cards = ["Guadalupe","Ollie","Aki"];
+
+function writeCards(names, descriptor){
+    const myArr= []
+    for(let i =0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${descriptor} gift!`);
+        console.log(names[i]);
+        myArr.push(`Thank you, ${names[i]}, for the wonderful ${descriptor} gift!`)
+    }
+    return myArr
+}
+ 
+writeCards(cards)
+
+function countDown(startingNum){
+    while ( startingNum >= 0){
+        console.log(startingNum)
+        startingNum--;
+    }
+    return startingNum
+}
